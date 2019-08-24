72e4eb9a0624d963d2c7262c39ce90f56ed4a8d3
HowardLei <410905481@qq.com>
2019-08-24 10:10:56 +0800

    解决登录后的界面以及逻辑

--- src/com/blogSystem/servlet/ListServlet.java
+++ src/com/blogSystem/servlet/ListServlet.java
@@ -1,8 +1,12 @@
 package com.blogSystem.servlet;
 
+import com.blogSystem.database.DB;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -13,12 +17,19 @@ import java.io.IOException;
  */
 @WebServlet(name = "ListServlet", description = "列举出所有数据", urlPatterns = {"/list"})
 public class ListServlet extends HttpServlet {
+    private static final String UTF_8_ENCODING = "utf-8";
+    private static final int LINE_LIMITS = 10;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding(UTF_8_ENCODING);
+        response.setCharacterEncoding(UTF_8_ENCODING);
+        response.setContentType("text/html;charset=utf-8");
+        var account = request.getSession().getAttribute("account");
+        var sql = String.format("select * from blog limit 0,%d", LINE_LIMITS);
+        var list = DB.select(sql);
+        list.forEach(System.out::println);
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
--- web/js/home.js
+++ web/js/home.js
@@ -1,6 +1,28 @@
 function addNewBlog() {
-    window.open("/BlogSystem/newBlog.html","__self")
+    window.open("/BlogSystem/html/newBlog.html","__self")
 }
 function logOut() {
-
+}
+function show() {
+    $.ajax({
+        url: "/BlogSystem/list",    //请求的url地址
+        dataType: "json",   // 返回格式为json
+        async: true,// 请求是否异步，默认为异步，这也是ajax重要特性
+        data: null, // 参数值
+        type: "post",  // 请求方式
+        "beforeSend": function () {
+            //请求前的处理
+        },
+        // 在后台处理该用户名是否存在以及能否添加
+        "success": function (req) {
+            //请求成功时处理
+        },
+        "complete": function () {
+            // 请求完成的处理
+        },
+        "error": function () {
+            // 请求出错处理
+            alert("出错了")
+        }
+    })
 }
