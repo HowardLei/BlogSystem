084e7cbc4f7a23b73ca5f3632fffeeee49b78d99
ITCenter <410905481@qq.com>
2019-08-24 21:21:47 +0800

    设置Blog的JavaBean

---
+++ src/com/blogSystem/entity/Blog.java
@@ -0,0 +1,78 @@
+package com.blogSystem.entity;
+
+import java.sql.Timestamp;
+
+public class Blog {
+    private String id;
+    private String title;
+    private String content;
+    private String author;
+    private Integer status;
+    private String type;
+    private Long userId;
+    private Timestamp createdTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Timestamp createdTime) {
+        this.createdTime = createdTime;
+    }
+}
