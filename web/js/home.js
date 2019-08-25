bf69d05b555b5fd81906479df884a71d3483f648
HowardLei <410905481@qq.com>
2019-08-25 17:29:41 +0800

    能查找出正确的数据。但是在创建table的时候按照需求执行。

--- web/js/home.js
+++ web/js/home.js
@@ -19,10 +19,6 @@ function show() {
         // 在后台处理该用户名是否存在以及能否添加
         "success": function (req) {
             //请求成功时处理
-            alert("成功获得值")
-            for (let i = 0; i < req.length; i++) {
-                console.log(req[i])
-            }
             showTable(req)
         },
         "complete": function () {
@@ -34,8 +30,9 @@ function show() {
     })
 }
 function showTable(req) {
-    var table = "<table>"
-    table += "<tr><td>标题</td><td>作者</td><td>修改时间</td></tr>>"
+    // FIXME: table 能够添加了，但是不能只创建 1 次 table。每次点击的时候都会
+    var table = "<table id='table'>"
+    table += "<tr><td>标题</td><td>作者</td><td>修改时间</td></tr>"
     for (let i = 0; i < req.length; i++) {
         table += "<tr><td>" + req[i]["title"] +
             "</td><td>" + req[i]["author"] +
