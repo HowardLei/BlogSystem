d4eb9c6a4807733374da22800445c66ebe33e855
HowardLei <410905481@qq.com>
2019-08-30 18:53:08 +0800

    添加编辑按钮

--- web/js/home.js
+++ web/js/home.js
@@ -37,15 +37,31 @@ function showTable(req) {
         table += "<tr><td>" + req[i]["title"] +
             "</td><td>" + req[i]["author"] +
             "</td><td>" + req[i]["createdTime"] +
+            "</td><td>" + "<input type='button' value='编辑' onclick='changeBlog()' id='" + i +"' />" +
             "</td></tr>"
     }
     table += "</table>"
     $("#table").empty().append(table)
 }
 function changeBlog() {
-    if (document.getElementById("table") == null) {
-        alert("对不起，没有博客数据，无法修改")
-    } else {
-
-    }
+    $.ajax({
+        url: "/BlogSystem/edit",    //请求的url地址
+        dataType: "json",   // 返回格式为json
+        async: true,// 请求是否异步，默认为异步，这也是ajax重要特性
+        data: null, // 参数值
+        type: "post",  // 请求方式
+        "beforeSend": function () {
+            //请求前的处理
+        },
+        // 在后台处理该用户名是否存在以及能否添加
+        "success": function (req) {
+            //请求成功时处理
+        },
+        "complete": function () {
+            // 请求完成的处理
+        },
+        "error": function () {
+            // 请求出错处理
+        }
+    })
 }
