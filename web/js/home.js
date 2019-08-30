1ec06fa9c94cf12c0bc1897502d1bc51df4a5313
HowardLei <410905481@qq.com>
2019-08-30 22:06:44 +0800

    在创建表格当中添加按钮来编辑里面的内容，但是方式不能用 ajax 。

--- web/js/home.js
+++ web/js/home.js
@@ -1,6 +1,3 @@
-function showText() {
-    document.getElementById("successMessage").innerText = ""
-}
 function addNewBlog() {
     window.open("/BlogSystem/html/newBlog.html","__self")
 }
@@ -37,18 +34,20 @@ function showTable(req) {
         table += "<tr><td>" + req[i]["title"] +
             "</td><td>" + req[i]["author"] +
             "</td><td>" + req[i]["createdTime"] +
-            "</td><td>" + "<input type='button' value='编辑' onclick='changeBlog()' id='" + i +"' />" +
+            "</td><td>" + "<input type='button' value='编辑' onclick='changeBlog(this)' id='" + (i + 1) +"' />" +
             "</td></tr>"
     }
     table += "</table>"
     $("#table").empty().append(table)
 }
-function changeBlog() {
+function changeBlog(element) {
+    let id = element.id
+    let data = {"id": id}
     $.ajax({
         url: "/BlogSystem/edit",    //请求的url地址
         dataType: "json",   // 返回格式为json
         async: true,// 请求是否异步，默认为异步，这也是ajax重要特性
-        data: null, // 参数值
+        data: data, // 参数值
         type: "post",  // 请求方式
         "beforeSend": function () {
             //请求前的处理
@@ -56,6 +55,9 @@ function changeBlog() {
         // 在后台处理该用户名是否存在以及能否添加
         "success": function (req) {
             //请求成功时处理
+            if (req["message"] == "200") {
+                window.open("123")
+            }
         },
         "complete": function () {
             // 请求完成的处理
